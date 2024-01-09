69463bc4bd9fe746250c2f82eab0b3b516e230d1
vanessa <nva.gomo926@gmail.com>
2024-01-09 11:00:35 -0500

    publicacion

--- Proyecto/WebApplication2/Models/MDistritos.cs
+++ Proyecto/WebApplication2/Models/MDistritos.cs
@@ -6,13 +6,13 @@
 		public string NOM_DIST { get; set; }
 		public string DETALLE { get; set; }
 		public string COMPUEST { get; set; }
-		public string ?COD_SUPERVISOR { get; set; }
-		public string ?COD_PRIMERPRESB { get; set; }
-		public string ?COD_SEGUNDOPRESB { get; set; }
-		public string ?COD_TERCERPRESB { get; set; }
-		public string ?COD_SECREPRESB { get; set; }
-		public string ?COD_TESOPRESB { get; set; }
-		public string ?COD_FISCAL { get; set; }
-		public string ?COD_ASESORCORPEN { get; set; }
+		public string? COD_SUPERVISOR { get; set; }
+		public string? COD_PRIMERPRESB { get; set; }
+		public string? COD_SEGUNDOPRESB { get; set; }
+		public string? COD_TERCERPRESB { get; set; }
+		public string? COD_SECREPRESB { get; set; }
+		public string? COD_TESOPRESB { get; set; }
+		public string? COD_FISCAL { get; set; }
+		public string? COD_ASESORCORPEN { get; set; }
 	}
 }
