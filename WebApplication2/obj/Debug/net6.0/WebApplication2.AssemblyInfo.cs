fd67d67272600e3abba4952c15d0ae5c6737a915
vanessa <nva.gomo926@gmail.com>
2024-01-23 15:17:24 -0500

    header de login .cshtml

--- WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c417a2b14ae95571c8aad494199284b5e0e4ff3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b59844b3f45a1b25dc12774f423d561a078a125")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
