cc7c307f039eea55441d28dd65593f7066daee64
vanessa <nva.gomo926@gmail.com>
2024-01-26 15:09:12 -0500

    publicacion con boton pdf

--- WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78b181a8557a474420a4469c692fb5ff42b8c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74895594b2ee5ea7dba5f76e3dbcc1432d2495f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
