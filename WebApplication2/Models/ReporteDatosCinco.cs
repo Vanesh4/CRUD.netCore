e508496653ee1b94f4e1cde1ab31491ff1302854
vanessa <nva.gomo926@gmail.com>
2024-03-01 16:37:10 -0500

    Casi todas las cuentas

--- WebApplication2/Models/ReporteDatosCinco.cs
+++ WebApplication2/Models/ReporteDatosCinco.cs
@@ -2,17 +2,17 @@
 {
     public class ReporteDatosCinco
     {
-        public List<Cinco> CajaGeneral { get; set; }
-        public List<Cinco> SegVicepresidente {  get; set; }
         public List<Cinco> AportesPastor { get; set; }
+        public List<Cinco> SegVicepresidente {  get; set; }
+        public List<Cinco> CajaMenor { get; set; }
+        public List<Cinco> CajaGeneral { get; set; }
+        public List<Cinco> BogotaCtasCorrientes { get; set; }
+        public List<Cinco> Otros { get; set; }
         public List<Cinco> Gastos { get; set; }
         public List<Cinco> GastosDirectivos { get; set; }
-        public List<Cinco> Otros { get; set; }
 
         public List<Cinco> TaxisyBuses { get; set; }
 
-        public List<Cinco> CajaMenor { get; set; }
-        public List<Cinco> BogotaCtasCorrientes { get; set; }
         public List<Cinco> InteresesCDT { get; set; }
         public List<Cinco> Reafiliaciones { get; set; }
     }
--- WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338324bccfda33369833e2094c857e7a16c2a993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2326aca48310324d247b38ed5e727afef42f3f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
