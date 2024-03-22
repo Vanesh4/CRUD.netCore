b96980e0a51ce17cb37dfca51574a87e2372c7cc
vanessa <nva.gomo926@gmail.com>
2024-03-22 16:33:15 -0500

    menu plantilla

--- WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8af5d4de20f2dc427d00eba34a29e49d5522242")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e319eb7351721b662464026177bf94e3221838f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
