eb47b4d9d36d0bbc0617336dc46417304c23b5e8
vanessa <nva.gomo926@gmail.com>
2024-04-01 15:46:31 -0500

    publicacion diseño plantilla login y cinco

--- WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96980e0a51ce17cb37dfca51574a87e2372c7cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd273d2e982a8e3e4382e003dcd93733a95e2c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
