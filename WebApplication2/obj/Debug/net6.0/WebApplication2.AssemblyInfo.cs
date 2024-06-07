d15453cccc29444a6fc83f5107f9d8bb29243a5c
Vanessa <nva.gomo926@gmail.com>
2024-06-07 15:32:50 -0500

    boton eliminar publicacion

--- WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc257418ff416cc7182c0d465816172ca7e51ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce46ecc13de4739e50ce6ee5335b783df0837bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Release/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6f10abb5dd28eb2181b3ad383925483b4bb3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce46ecc13de4739e50ce6ee5335b783df0837bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
