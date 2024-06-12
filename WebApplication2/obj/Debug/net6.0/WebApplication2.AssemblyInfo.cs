bf60ba306a92ca7c2abfa9e8ae579de48fb32b4a
Vanessa <nva.gomo926@gmail.com>
2024-06-12 16:11:43 -0500

    verificar fecha completo

--- WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048e78097d1e6186adc8a4aa720678d6c5fa73dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1b9cdb193afe914b8865b103b51f58fbdb1808")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
