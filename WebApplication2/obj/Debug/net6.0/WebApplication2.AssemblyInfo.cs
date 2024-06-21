a39b66a700d0d9fdaa78262b4fb399200d5d028f
Vanessa <nva.gomo926@gmail.com>
2024-06-21 16:35:43 -0500

    inicio diseño pdf

--- WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf2b66e159b17d1fde004ad28446c8dea83f361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad2e431ddbd36f66114446fbd837c458fc06da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
